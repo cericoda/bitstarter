1c101fdf44824d0d145d0b1afcba2e83da21b9cb
cericoda <cericoda@altfa.com>
2013-08-07 20:54:58 +0000

    edited title

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +36,7 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +63,41 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(htmlFile, checkFile) {
+    var response2HtmlFile = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            //console.error("Wrote %s", htmlFile);
+            fs.writeFileSync(htmlFile, result);
+	    var checkJson = checkHtmlFile(htmlFile, checkFile);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(checkJson);
+        }
+    };
+    return response2HtmlFile;
+};
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u --url <check_url>', 'Path to web resource')
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	// write file to tmp
+	program.file = 'urlResult.html';
+	// console.log(program.file);
+	var response2HtmlFile = buildfn('tmpurlResult.html', program.checks);
+	rest.get(program.url).on('complete', response2HtmlFile);
+
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(checkJson);
+	};
+   // console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
